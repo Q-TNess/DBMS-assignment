52ab4c1eea5b8df2ff390e43683d5ad50c346475
Q-TNess <mpumelaqq@gmail.com>
2025-03-19 23:18:45 +0200

    Add files via upload
    DBMS assignment complete.

---
+++ answers.sql
@@ -0,0 +1,17 @@
+-- Creating a database called salesDB in Command prompt you type 
+--the following command to log into MySQL
+mysql -u root -p
+
+--use your MySQL root password when prompted
+
+-- Then create the database by typing the following as is
+CREATE DATABASE salesDB;
+
+-- To test it run the query in command prompt  by plugging the following
+SHOW DATABASES;
+
+--Follow the same steps to delete the DB but use DROP instead of CREATE
+-- Drop (delete) the database called demo by typing the following
+DROP DATABASE demo;
+
+--SHOW DATABASES to verify deletion
